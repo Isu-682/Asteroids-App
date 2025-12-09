ad9cca72248a7927dcf6a4c296cb1fd691ecc2bd
Isu-682 <padillamachayariana@gmail.com>
2025-12-09 01:41:15 -0600

    Agregar archivos de proyecto.

---
+++ Asteroids App/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Asteroids_App")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Asteroids_App.Pages")]
---
+++ Asteroids App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Asteroids_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
