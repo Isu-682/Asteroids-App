af6f52a40c8344d8b247e5d48054cc26fae092ab
Isu-682 <padillamachayariana@gmail.com>
2025-12-09 01:49:36 -0600

    se crea otra clase llamada NeoServices dentro de la carpeta Services donde se pidio una Api Key en la pagina de la Nasa que me proporciono

---
+++ Asteroids App/Services/NeoService.cs
@@ -0,0 +1,48 @@
+﻿using Asteroids_App.Models;
+using System.Text.Json;
+
+namespace Asteroids_App.Services
+{
+    public class NeoService
+    {
+        private const string ApiKey = "f1fDCyYt7LcJieTMdXSZDbycinJCX6sYO1WOvZcW";
+        private readonly HttpClient _httpClient;
+
+        public NeoService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Neo>> GetNeoFeedAsync(string startDate, string endDate)
+        {
+            var url = $"https://api.nasa.gov/neo/rest/v1/feed?start_date={startDate}&end_date={endDate}&api_key={ApiKey}";
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            using var doc = JsonDocument.Parse(json);
+
+            var neos = new List<Neo>();
+            var nearEarthObjects = doc.RootElement.GetProperty("near_earth_objects");
+
+            foreach (var date in nearEarthObjects.EnumerateObject())
+            {
+                foreach (var item in date.Value.EnumerateArray())
+                {
+                    var neo = new Neo
+                    {
+                        Name = item.GetProperty("name").GetString(),
+                        EstimatedDiameterMin = item.GetProperty("estimated_diameter").GetProperty("kilometers").GetProperty("estimated_diameter_min").GetDouble(),
+                        EstimatedDiameterMax = item.GetProperty("estimated_diameter").GetProperty("kilometers").GetProperty("estimated_diameter_max").GetDouble(),
+                        IsPotentiallyHazardous = item.GetProperty("is_potentially_hazardous_asteroid").GetBoolean(),
+                        CloseApproachDate = item.GetProperty("close_approach_data")[0].GetProperty("close_approach_date").GetString(),
+                        MissDistanceKm = double.Parse(item.GetProperty("close_approach_data")[0].GetProperty("miss_distance").GetProperty("kilometers").GetString())
+                    };
+                    neos.Add(neo);
+                }
+            }
+
+            return neos;
+        }
+    }
+}
