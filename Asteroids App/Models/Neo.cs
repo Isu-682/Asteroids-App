dc548dbc87ada7eb239e8fb8a98db4c9ed143347
Isu-682 <padillamachayariana@gmail.com>
2025-12-09 01:43:58 -0600

    Se crea repositorio de los Asteroides NeoWs creandose la clase de Neo dentro de una carpeta llamada Models

---
+++ Asteroids App/Models/Neo.cs
@@ -0,0 +1,12 @@
+﻿namespace Asteroids_App.Models
+{
+    public class Neo
+    {
+        public string Name { get; set; }
+        public double EstimatedDiameterMin { get; set; }
+        public double EstimatedDiameterMax { get; set; }
+        public bool IsPotentiallyHazardous { get; set; }
+        public string CloseApproachDate { get; set; }
+        public double MissDistanceKm { get; set; }
+    }
+}
